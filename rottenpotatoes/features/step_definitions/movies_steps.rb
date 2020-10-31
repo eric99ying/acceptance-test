6a29b1678d919dcb56a40ed14118de8e0d59f7df
Eric Ying <eying@berkeley.edu>
2020-10-31 21:56:56 +0000

    do 8.9

--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -23,3 +23,8 @@ Then /I should see all the movies/ do
     step %{I should see "#{movie.title}"}
   end
 end
+
+Then /the director of "(.*)" should be "(.*)"/ do |m, d|
+  found = Movie.find_by(:title => m)
+  expect(found.director).to eq(d)
+end
